2e5173faffeccab0ba286bfb3f9bd9a6500c559e
Pascal <pascalmerme@no-log.org>
2015-02-09 18:29:07 +0100

    testing

--- src/android/hobnobGeolocation.java
+++ src/android/hobnobGeolocation.java
@@ -1,7 +1,7 @@
 package org.apache.cordova.plugin;
 
 import org.apache.cordova.CordovaPlugin;
-import org.apache.cordova.PluginResult;
+import org.apache.cordova.CallbackContext;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
