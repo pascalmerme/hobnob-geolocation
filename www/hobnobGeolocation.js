20faf9865e322866b73214643e8cd7b3313a84e6
Pascal <pascalmerme@no-log.org>
2015-02-09 18:33:31 +0100

    testing

--- www/hobnobGeolocation.js
+++ www/hobnobGeolocation.js
@@ -1,13 +1,13 @@
 module.exports = {
     start: function(success, failure, config) {
-        exec(success || function() {},
+        cordova.exec(success || function() {},
              failure || function() {},
              'HobnobGeolocation',
              'start',
              []);
     },
     stop: function(success, failure, config) {
-        exec(success || function() {},
+        cordova.exec(success || function() {},
             failure || function() {},
             'HobnobGeolocation',
             'stop',
