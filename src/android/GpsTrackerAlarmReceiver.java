d5f4d96745076f8746fd3700a6e151ce7c8a940c
Pascal <pascalmerme@no-log.org>
2015-02-10 16:50:59 +0100

    Debugging

--- src/android/GpsTrackerAlarmReceiver.java
+++ src/android/GpsTrackerAlarmReceiver.java
@@ -3,6 +3,7 @@ package in.hobnob;
 import android.support.v4.content.WakefulBroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 
 public class GpsTrackerAlarmReceiver extends WakefulBroadcastReceiver {
     private static final String TAG = "GpsTrackerAlarmReceiver";
